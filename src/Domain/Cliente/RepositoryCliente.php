11a7ce29eb7830199411e2c09954e00d683d1872
Julio Mafra <julioclmafra@gmail.com>
2023-06-01 13:55:16 -0300

    alteracoes

--- src/Domain/Cliente/RepositoryCliente.php
+++ src/Domain/Cliente/RepositoryCliente.php
@@ -4,13 +4,9 @@ namespace Julio\Projeto\Domain\Cliente;
 
 interface RepositoryCliente
 {
-
     public function list() : array;
     public function add(Cliente $cliente): bool;
-
     public function search(string $email): Cliente;
-
     public function remove(string $email): bool;
-
     public function edit(string $email, Cliente $cliente): bool;
 }
